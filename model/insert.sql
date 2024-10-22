51f69cdae60f844c16d04c3d17b8bfed198bedea
muhamadzaiinizeebindaivin <muhamad.bin-daivin@etu.univ-orleans.fr>
2024-10-22 14:56:19 +0200

    Implémentation des triggers et des fonctions

--- model/insert.sql
+++ model/insert.sql
@@ -28,6 +28,7 @@ INSERT INTO ENTREPRISE (nom_Entreprise) VALUES ('Entreprise C');
 INSERT INTO UTILISATEUR (nom_Utilisateur, mail, numtel, motdepasse, id_Entreprise) VALUES ('Utilisateur 1', 'utilisateur1@example.com', 1234567890, 'password1', 1);
 INSERT INTO UTILISATEUR (nom_Utilisateur, mail, numtel, motdepasse, id_Entreprise) VALUES ('Utilisateur 2', 'utilisateur2@example.com', 1234567891, 'password2', 2);
 INSERT INTO UTILISATEUR (nom_Utilisateur, mail, numtel, motdepasse, id_Entreprise) VALUES ('Utilisateur 3', 'utilisateur3@example.com', 1234567892, 'password3', 3);
+INSERT INTO UTILISATEUR (nom_Utilisateur, mail, numtel, motdepasse, id_Entreprise) VALUES ('Utilisateur 4', 'utilisateur4@example.com', 282567892, 'password3', NULL);
 
 INSERT INTO POINT_DE_COLLECTE (Adresse, pos_x, pos_y, qte_max) VALUES ('Adresse 1', 12.3456, 78.9012, 100);
 INSERT INTO POINT_DE_COLLECTE (Adresse, pos_x, pos_y, qte_max) VALUES ('Adresse 2', 23.4567, 89.0123, 200);
@@ -36,19 +37,32 @@ INSERT INTO POINT_DE_COLLECTE (Adresse, pos_x, pos_y, qte_max) VALUES ('Adresse 
 INSERT INTO COLLECTE (Date_Collecte) VALUES ('2024-01-01');
 INSERT INTO COLLECTE (Date_Collecte) VALUES ('2024-02-01');
 INSERT INTO COLLECTE (Date_Collecte) VALUES ('2024-03-01');
+INSERT INTO COLLECTE (Date_Collecte) VALUES ('2024-04-01');
 
 INSERT INTO DECHET (nom_Dechet, id_Type, qte) VALUES ('Déchet 1', 1, 10);
 INSERT INTO DECHET (nom_Dechet, id_Type, qte) VALUES ('Déchet 2', 2, 20);
 INSERT INTO DECHET (nom_Dechet, id_Type, qte) VALUES ('Déchet 3', 3, 30);
 INSERT INTO DECHET (nom_Dechet, id_Type, qte) VALUES ('Déchet 4', 3, 30);
+INSERT INTO DECHET (nom_Dechet, id_Type, qte) VALUES ('Déchet 5', 1, 500);
+INSERT INTO DECHET (nom_Dechet, id_Type, qte) VALUES ('Déchet 6', 2, 50);
+
 
 
 INSERT INTO DEPOSER (id_Dechet, id_Utilisateur, id_point_collecte) VALUES (1, 1, 1);
 INSERT INTO DEPOSER (id_Dechet, id_Utilisateur, id_point_collecte) VALUES (2, 2, 2);
 INSERT INTO DEPOSER (id_Dechet, id_Utilisateur, id_point_collecte) VALUES (3, 3, 3);
 INSERT INTO DEPOSER (id_Dechet, id_Utilisateur, id_point_collecte) VALUES (4, 3, 3);
+INSERT INTO DEPOSER (id_Dechet, id_Utilisateur, id_point_collecte) VALUES (6, 1, 3);
+
+
+--INSERTION POUR TESTER LE TRIGGER check_qte_deposee
+--INSERT INTO DEPOSER (id_Dechet, id_Utilisateur, id_point_collecte) VALUES (5, 2, 3);
 
 
 INSERT INTO TRAITER (id_point_collecte, id_Collecte, id_Type, qtecollecte) VALUES (1, 1, 1, 5);
 INSERT INTO TRAITER (id_point_collecte, id_Collecte, id_Type, qtecollecte) VALUES (2, 2, 2, 15);
 INSERT INTO TRAITER (id_point_collecte, id_Collecte, id_Type, qtecollecte) VALUES (3, 3, 3, 25);
+INSERT INTO TRAITER (id_point_collecte, id_Collecte, id_Type, qtecollecte) VALUES (3, 4, 3, 61);
+
+--INSERTION POUR TESTER LE TRIGGER check_qte_collecte
+INSERT INTO TRAITER (id_point_collecte, id_Collecte, id_Type, qtecollecte) VALUES (3, 4, 3, 50);

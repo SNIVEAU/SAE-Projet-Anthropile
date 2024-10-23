be77a302ff6544801a27eac1770680fc9bc2670d
NiksanNagarajah <niksan.nagarajah@etu.univ-orleans.fr>
2024-10-23 14:31:28 +0200

    insertion déchets via class

--- app/models.py
+++ app/models.py
@@ -8,13 +8,45 @@ from .app import mysql
 #     print(id_max, "*********")
 #     return id_max
 
+class CategorieDechet:
+    def __init__(self, id_type, nom_type):
+        self.id_type = id_type
+        self.nom_type = nom_type
+
+    def __repr__(self):
+        return self.nom_type
+
 def get_categories():
     cursor = mysql.connection.cursor()
     cursor.execute("SELECT * FROM CATEGORIEDECHET")
     categories = cursor.fetchall()
     cursor.close()
-    print(categories)
-    return categories
+    les_categories = []
+    for id_categorie, nom_categorie in categories:
+        les_categories.append(CategorieDechet(id_categorie, nom_categorie))
+    for categorie in les_categories:
+        print(type(categorie))
+    print(categories, les_categories)
+    # return categories
+    return les_categories
+
+class Dechet:
+    def __init__(self, nom_dechet, id_type, quantite):
+        self.nom_dechet = nom_dechet
+        if type(id_type) == str:
+            self.id_type = get_id_type_dechet(id_type)
+        else:
+            self.id_type = id_type
+        self.quantite = quantite
+
+    def __repr__(self):
+        return self.nom_dechet
+    
+    def insert_dechet(self):
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO DECHET(nom_Dechet, id_Type, qte) VALUES (%s, %s, %s)", (self.nom_dechet, self.id_type, self.quantite))
+        mysql.connection.commit()
+        cursor.close()
 
 def get_id_type_dechet(nom_dechet):
     cursor = mysql.connection.cursor()
